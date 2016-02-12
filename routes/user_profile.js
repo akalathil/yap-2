3756b39e2ffa08782a56e7e63defc69eff438f04
Arvind Kalathil <akalathi@ucsd.edu>
2016-02-11 23:52:29 -0800

    Made it so that when clicking on any professor it can change to that professors page. Lots of fixes need to be made

--- routes/user_profile.js
+++ routes/user_profile.js
@@ -2,6 +2,24 @@
 var data = require("../footer.json");
 
 exports.getprofile = function(req, res){
-	console.log(data);
-	res.render('user_profile', data);
+
+	/* GENERALIZE THIS JSON READING */
+	var users = data["teach_education_listing"];
+
+	var userData;
+	var index = 0;
+
+	while (true) {
+		var currentPerson = users[index];
+		if( currentPerson.name == req.query.name){
+			userData = users[index];
+			console.log(userData);
+			break;
+		}
+		index++;
+	}
+	/*console.log(data);*/
+	res.render('user_profile', userData);
 }
+
+
