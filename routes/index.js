e10ae1d0fcd9a2f1d2ec4b0c3fc9b5ffff092298
Chonlada Woointranon <cwoointr@gmail.com>
2016-02-28 21:00:35 -0800

    Index.js Error when trying to include jQuery.
    /Users/pam/git/yap/routes/index.js:21
    $(document).ready(function() {
    ^
    ReferenceError: $ is not defined
        at Object.<anonymous> (/Users/pam/git/yap/routes/index.js:21:1)
        at Module._compile (module.js:460:26)
        at Object.Module._extensions..js (module.js:478:10)
        at Module.load (module.js:355:32)
        at Function.Module._load (module.js:310:12)
        at Module.require (module.js:365:17)
        at require (module.js:384:17)
        at Object.<anonymous> (/Users/pam/git/yap/app.js:11:13)
        at Module._compile (module.js:460:26)
        at Object.Module._extensions..js (module.js:478:10)

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // Get all of our friend data
 var dataJson = require('../footer.json');
 
@@ -14,3 +16,21 @@ exports.login = function(req, res){
 	//console.log(data);
 	res.render('login', data);
 };
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+});
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	$(".fa-angle-double-left").click(backButtonClicked);
+}
+
+function backButtonClicked(e){
+	e.preventDefault();
+	console.log("Back button clicked")
+	ga("send", "event", "back", "click");
+}
