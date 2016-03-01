b1ff006bfee7de475e287c5814c3392e6240aa7a
Chonlada Woointranon <cwoointr@gmail.com>
2016-02-29 19:59:58 -0800

    Added Back Button Experiment on GA

--- routes/index.js
+++ routes/index.js
@@ -16,21 +16,3 @@ exports.login = function(req, res){
 	//console.log(data);
 	res.render('login', data);
 };
-
-// Call this function when the page loads (the "ready" event)
-$(document).ready(function() {
-	initializePage();
-});
-
-/*
- * Function that is called when the document is ready.
- */
-function initializePage() {
-	$(".fa-angle-double-left").click(backButtonClicked);
-}
-
-function backButtonClicked(e){
-	e.preventDefault();
-	console.log("Back button clicked")
-	ga("send", "event", "back", "click");
-}
