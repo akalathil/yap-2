9f7db412128cac0abc1480ecc457b7290756e01c
Chonlada Woointranon <cwoointr@gmail.com>
2016-03-08 12:02:05 -0800

    Added Dollar Signs

--- public/js/yap.js
+++ public/js/yap.js
@@ -13,6 +13,7 @@ function initializePage() {
 	$('#fb').click(login);
     $('#google').click(login);
 	$(".fa-angle-double-left").click(backButtonClicked);
+	$(".fa-search").click(searchFn);
 }
 function login(e) {
 	// Prevent following the link
@@ -24,7 +25,29 @@ function login(e) {
     document.location.href = currURL+'home';
 }
 
-function backButtonClicked(e){
+function backButtonClicked(e) {
 	console.log("Back button clicked")
 	ga("send", "event", "back", "click");
+}
+
+function searchFn(e) {
+
+	var data = {};
+	data["footer"] = dataFooter;
+
+	var listings = data["footer"]['teach_education_listing'];
+	var searchTerm = $("#search-text").val();
+	console.log("Search Term is: ")
+	console.log(searchTerm)
+	var result = [];
+
+	listings.map(function (obj) {
+		for (var prop in obj) {
+			if (obj[prop].toLowerCase().indexOf(searchTerm.toLowerCase()) > -1) {
+				result.push(obj);
+				return 0;
+			}
+		}
+		return 0;
+	});
 }
