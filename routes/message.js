f5960e908255667470c3ef446a84123095b419a3
Chonlada Woointranon <cwoointr@gmail.com>
2016-03-08 21:46:05 -0800

    Added Title and Taylor Message

--- routes/message.js
+++ routes/message.js
@@ -4,8 +4,6 @@ var dataFooter = require('../footer.json');
 exports.messenger = function(req, res){
     var data = {}
     data["footer"] = dataFooter;
-	//console.log(data);
-	//console.log("BYE")
 	res.render('messenger', data);
 };
 
@@ -13,7 +11,10 @@ exports.message = function(req, res){
     var data = {}
     data["footer"] = dataFooter;
 	var specificMessage = data["footer"]["message"][req.params.name];
+    console.log(req)
+    console.log(req.params)
+    console.log(req.params.name)
     data["message"] = specificMessage;
 	res.render('message', data);
-    console.log(data["message"]);
+    //console.log(data["message"]);
 };
