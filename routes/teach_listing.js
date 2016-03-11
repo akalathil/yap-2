295e56cfa41ffffdcdc58d39002b6c0abea0a4e8
Chonlada Woointranon <cwoointr@gmail.com>
2016-03-10 18:48:51 -0800

    Search!

--- routes/teach_listing.js
+++ routes/teach_listing.js
@@ -24,9 +24,45 @@ exports.beauty = function(req, res) {
 	data["footer"] = dataFooter;
 	res.render('teach_listing_beauty', data);
 }
+
 exports.search = function(req, res) {
 	var data = {};
+	var searchTerm = '';
 	data["footer"] = dataFooter;
+
+	if (req.query && req.query.search) {
+		searchTerm = req.query.search;
+	}
+
+	console.log(searchTerm, "searchTerm");
+
+	if (!searchTerm) {
+		res.render('teach_listing_search', data);
+	} else {
+		var listings = data["footer"]['teach_education_listing'];
+		listings = listings.concat(data["footer"]['teach_music_listing'])
+		console.log("LALALA")
+		// console.log(listings)
+		var result = [];
+
+		listings.map(function (obj) {
+			for (var prop in obj) {
+				if (obj[prop].toLowerCase().indexOf(searchTerm.toLowerCase()) > -1) {
+					result.push(obj);
+					return 0;
+				}
+			}
+			return 0;
+		});
+		data.result = result;
+		res.render('teach_listing_search', data);
+		console.log(data.result, 'result');
+	}
+}
+
+exports.searchResult = function(req, res) {
+	var searchData = {};
+	searchData["footer"] = dataFooter;
 	var searchTerm = '';
 
 	if (req.query && req.query.search) {
@@ -35,10 +71,13 @@ exports.search = function(req, res) {
 
 	console.log(req.query.search);
 
-	var listings = data["footer"]['teach_education_listing'];
+	var listings = searchData["footer"]['teach_education_listing'];
+	listings = listings.concat(searchData["footer"]['teach_music_listing'])
+	console.log("LALALA")
+	console.log(listings)
 	// var searchTerm = $("#search-text").val();
-	console.log("Search Term is: ")
-	console.log(searchTerm)
+	//console.log("Search Term is: ")
+	//console.log(searchTerm)
 	var result = [];
 
 	listings.map(function (obj) {
@@ -50,5 +89,5 @@ exports.search = function(req, res) {
 		}
 		return 0;
 	});
-	res.render('teach_listing_search', data);
+	res.render('teach_listing_search', searchData);
 }
